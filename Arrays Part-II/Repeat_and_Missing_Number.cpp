05c646b19f5d5248767d8f8ec823bc7438b5d05b
Prakhar Singh <92270107+prakhar-singh09@users.noreply.github.com>
2023-09-25 19:36:33 +0530

    Create Repeat_and_Missing_Number.cpp

---
+++ Arrays Part-II/Repeat_and_Missing_Number.cpp
@@ -0,0 +1,38 @@
+// You are given a read-only array of N integers with values also in the range [1, N] both inclusive. Each integer appears exactly once except A which appears twice and B which is missing. 
+// The task is to find the repeating and missing numbers A and B where A repeats twice and B is missing.
+// Input:[3 1 2 5 3] 
+
+// Output:[3, 4] 
+
+// A = 3, B = 4
+
+vector<int> repeatedNumber(const vector<int> &a) {
+     long long n = a.size(); // size of the array
+
+    // Find Sn and S2n:
+    long long SN = (n * (n + 1)) / 2;
+    long long S2N = (n * (n + 1) * (2 * n + 1)) / 6;
+
+    // Calculate S and S2:
+    long long S = 0, S2 = 0;
+    for (int i = 0; i < n; i++) {
+        S += a[i];
+        S2 += (long long)a[i] * (long long)a[i];
+    }
+
+    //S-Sn = X-Y:
+    long long val1 = S - SN;
+
+    // S2-S2n = X^2-Y^2:
+    long long val2 = S2 - S2N;
+
+    //Find X+Y = (X^2-Y^2)/(X-Y):
+    val2 = val2 / val1;
+
+    //Find X and Y: X = ((X+Y)+(X-Y))/2 and Y = X-(X-Y),
+    // Here, X-Y = val1 and X+Y = val2:
+    long long x = (val1 + val2) / 2;
+    long long y = x - val1;
+
+    return {(int)x, (int)y};
+}
