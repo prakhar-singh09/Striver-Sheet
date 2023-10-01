9962acfaa28719fffe08db5b89050f60880bd61c
Prakhar Singh <92270107+prakhar-singh09@users.noreply.github.com>
2023-10-01 14:56:55 +0530

    Create Search_in_a_2D_matrix.cpp

---
+++ Arrays Part - III/Search_in_a_2D_matrix.cpp
@@ -0,0 +1,36 @@
+// You are given an m x n integer matrix matrix with the following two properties:
+// Each row is sorted in non-decreasing order.
+// The first integer of each row is greater than the last integer of the previous row.
+// Given an integer target, return true if target is in matrix or false otherwise.
+// You must write a solution in O(log(m * n)) time complexity.
+
+
+// Example 1:
+// Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+// Output: true
+
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int left = 0;
+        int right = m * n - 1;
+
+        while (left <= right) {
+            int mid = (right + left) / 2;
+            int midElement = matrix[mid / n][mid % n];
+
+            if (midElement == target) {
+                return true;
+            } else if (midElement < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+};
