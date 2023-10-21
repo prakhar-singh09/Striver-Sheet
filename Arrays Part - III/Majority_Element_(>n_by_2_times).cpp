cc9a2ed1078646f9db78d8527eeccc133c6af25b
Prakhar Singh <92270107+prakhar-singh09@users.noreply.github.com>
2023-10-21 10:55:55 +0530

    Create Majority_Element_(>n_by_2_times).cpp

---
+++ Arrays Part - III/Majority_Element_(>n_by_2_times).cpp
@@ -0,0 +1,34 @@
+// Given an array nums of size n, return the majority element.
+
+// The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+
+// Example 1:
+
+// Input: nums = [3,2,3]
+// Output: 3
+// Example 2:
+
+// Input: nums = [2,2,1,1,1,2,2]
+// Output: 2
+
+Code-
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+   //Moore algo
+   int cur = nums[0];
+   int count = 1;
+   for(int i=1;i<nums.size();i++){
+       if(cur != nums[i])
+       count--;
+       else count++;
+   if(count==0){
+       cur = nums[i];
+       count++;
+     }
+   }
+      return cur;
+    }
+};
