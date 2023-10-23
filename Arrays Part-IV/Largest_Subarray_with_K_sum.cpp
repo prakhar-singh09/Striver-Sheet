ad1fcec0792e1056e79d866befc630add80ad8da
Prakhar Singh <92270107+prakhar-singh09@users.noreply.github.com>
2023-10-23 13:18:11 +0530

    Create Largest_Subarray_with_K_sum.cpp

---
+++ Arrays Part-IV/Largest_Subarray_with_K_sum.cpp
@@ -0,0 +1,44 @@
+// Problem Statement: Given an array containing both positive and negative integers, we have to find the length of the longest subarray with the sum of all elements equal to zero.
+// Examples
+// Example 1:
+// Input Format: N = 6, array[] = {9, -3, 3, -1, 6, -5}
+// Result: 5
+// Explanation: The following subarrays sum to zero:
+// {-3, 3} , {-1, 6, -5}, {-3, 3, -1, 6, -5}
+// Since we require the length of the longest subarray, our answer is 5!
+
+// Example 2:
+// Input Format: N = 8, array[] = {6, -2, 2, -8, 1, 7, 4, -10}
+// Result: 8
+// Subarrays with sum 0 : {-2, 2}, {-8, 1, 7}, {-2, 2, -8, 1, 7}, {6, -2, 2, -8, 1, 7, 4, -10}
+// Length of longest subarray = 8
+
+//CODE
+
+int maxLen(int A[], int n)
+{
+    // Your code here
+    unordered_map<int,int> mpp; 
+    int maxi = 0;
+    int sum = 0; 
+    for(int i = 0;i<n;i++) {
+        sum += A[i]; 
+        if(sum == 0) {
+            maxi = i + 1; 
+        }
+        else {
+            if(mpp.find(sum) != mpp.end()) {
+                maxi = max(maxi, i - mpp[sum]); 
+            }
+            else {
+                mpp[sum] = i;
+            }
+        }   
+    }
+
+    return maxi; 
+}
+
+// Time Complexity: O(N), as we are traversing the array only once
+
+// Space Complexity: O(N), in the worst case we would insert all array elements prefix sum into our hashmap
