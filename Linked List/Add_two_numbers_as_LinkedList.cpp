2ccf6409cd4531ca98665efb35f8c354e40c5d7a
Prakhar Singh <92270107+prakhar-singh09@users.noreply.github.com>
2023-11-10 18:26:00 +0530

    Create Add_two_numbers_as_LinkedList.cpp

---
+++ Linked List/Add_two_numbers_as_LinkedList.cpp
@@ -0,0 +1,55 @@
+// // You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+// // You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+ 
+
+// // Example 1:
+
+
+// // Input: l1 = [2,4,3], l2 = [5,6,4]
+// // Output: [7,0,8]
+// // Explanation: 342 + 465 = 807.
+// // Example 2:
+
+// // Input: l1 = [0], l2 = [0]
+// // Output: [0]
+// // Example 3:
+
+// // Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+// // Output: [8,9,9,9,0,0,0,1]
+
+// Solution
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode *dummy = new ListNode(); 
+        ListNode *temp = dummy; 
+        int carry = 0;
+        while( (l1 != NULL || l2 != NULL) || carry) {
+            int sum = 0; 
+            if(l1 != NULL) {
+                sum += l1->val; 
+                l1 = l1 -> next; 
+            }
+            
+            if(l2 != NULL) {
+                sum += l2 -> val; 
+                l2 = l2 -> next; 
+            }
+            
+            sum += carry; 
+            carry = sum / 10; 
+            ListNode *node = new ListNode(sum % 10); 
+            temp -> next = node; 
+            temp = temp -> next; 
+        }
+        return dummy -> next; 
+    }
+};
+
+
+// Time Complexity: O(max(m,n)). Assume that m and n represent the length of l1 and l2 respectively, the algorithm above iterates at most max(m,n) times.
+
+// Space Complexity: O(max(m,n)). The length of the new list is at most max(m,n)+1.
